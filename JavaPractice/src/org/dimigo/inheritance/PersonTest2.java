ae40120dd814dc9b16a169c4a4bcadba2e95e34c
RMKroar <rmkroar97@gmail.com>
2017-05-10 12:31:28 +0900

    실습과제 제출 10

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,36 @@
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *  |_ PersonTest2.java
+ * 
+ * Date : 2017. 5. 10.
+ * This program is for practicing how to utilize method overriding in Java.
+ * </pre>
+ *
+ * @author : RMKroar
+ * @version : 1.0
+ */
+
+public class PersonTest2 {
+	public static void main(String[] args) {
+		Person[] person = {
+			new Person("Tom"),
+			new Korean("홍길동"),
+			new Japanese("다나카"),
+			new Chinese("왕밍")
+		};
+		
+		for(Person p : person) {
+			greeting(p);
+		}
+	}
+	
+	private static void greeting(Person p) {
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
+	}
+}
