3d5dab2821edf9fe09dc57e0220204ebbbe9d762
RMKroar <rmkroar97@gmail.com>
2017-05-12 15:50:10 +0900

    실습과제 제출 11 수정

--- JavaPractice/src/org/dimigo/inheritance/Galaxy.java
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -23,6 +23,6 @@ public class Galaxy extends SmartPhone {
 		System.out.println("삼성 페이로 결제합니다.");
 	}
 	public void useWirelessCharging() {
-		System.out.println("무선 충전 기능을 사용합니다.");
+		System.out.println("무선 충전 기능을 이용합니다.");
 	}
 }
