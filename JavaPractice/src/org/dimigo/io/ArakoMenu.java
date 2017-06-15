c02cce27880cb3a17b410f333934dcb2fa65ab22
rmkro <rmkro@DESKTOP-M6VQNGI>
2017-06-15 04:24:38 -0700

    실습과제 제출 17

---
+++ JavaPractice/src/org/dimigo/io/ArakoMenu.java
@@ -0,0 +1,28 @@
+/* Formatted - */
+
+package org.dimigo.io;
+
+import java.io.*;
+
+public class ArakoMenu {
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			Writer writer = new BufferedWriter(new FileWriter("files/menu.txt"));
+			BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))) {
+			
+			String data;
+			while((data = br.readLine()) != null) {
+				writer.write(data + "\n");
+			}
+			
+			writer.flush();
+			
+			System.out.println(" << 메뉴 출력 >> ");
+			while((data = reader.readLine()) != null) {
+				System.out.println(data);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
